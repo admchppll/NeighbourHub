3fc0cfa603115468f928e0ee620ed47a0a065244
Adam Chappell <adam@adamchappell.com>
2017-04-11 15:05:48 +0100

    Can't see volunteer/withdraw button unless logged in
    -Fixed button issue with event details on search page

--- Website/Community/Community/Controllers/VolunteerController.cs
+++ Website/Community/Community/Controllers/VolunteerController.cs
@@ -62,8 +62,6 @@ namespace Community.Controllers
         }
 
         // POST: Volunteer/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,EventID,VolunteerID,Confirmed,Rejected,Withdrawn")] Volunteer volunteer)
